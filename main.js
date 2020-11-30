17da9f7bd6c1da54caeed106b6451da96a869c70
Cody Meireis <codymeireis@gmail.com>
2020-11-30 12:50:07 -0600

    feature: adding mutate method

--- main.js
+++ main.js
@@ -12,3 +12,21 @@ const mockUpStrand = () => {
   }
   return newStrand
 }
+
+const pAequorFactory = (number, arrayDnaBases) => {
+  const specObj = {
+    "specimenNum": number,
+    "dna": arrayDnaBases,
+    mutate = () => {
+      const randNum = Math.floor(Math.random() * this.arrayDnaBases.length);
+      let newBase = returnRandBase();
+      while (this.arrayDnaBases[randNum] === newBase) {
+        newbase = returnRandBase;
+      }
+        this.arrayDnaBases[randNum] = newBase;
+        return this.arrayDnaBases;
+    }
+  };
+};
+
+//console.log(pAequorFactory(1, mockUpStrand()));
